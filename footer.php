9a0716b0c6e56ca5c58f81e86b72ef6efb64a9e2
manish <support@technofra.com>
2026-03-04 11:33:43 +0530

    gdfsgsdfgg

--- footer.php
+++ footer.php
@@ -1,146 +1,143 @@
 <!--===== FOOTER AREA STARTS =======-->
 <div class="footer1">
-    <div class="container">
+   <div class="container">
       <div class="row">
          <div class="col-xl-3 col-lg-4 col-md-6">
-              <div class="footer-area-logo">
-                  <img src="assets/img/logo/home-01-header-logo.png" alt="">
-                  <div class="space16"></div>
-                  <p>At Oceanic, we’re more than just painterspassionate professionals committed to quality, reliability, and beautiful results. Whether it’s one room or your entire.</p>
-                  <div class="space24"></div>
-                  <ul class="social_link">
-                    <li>
-                      <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
-                    </li>
-                    <li>
-                       <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fa-brands fa-youtube"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                    </li>
-                  </ul>
-              </div>
+            <div class="footer-area-logo">
+               <img src="assets/img/logo/home-01-header-logo.png" alt="">
+               <div class="space16"></div>
+               <p>Oceanic is focused on high-performance paint systems for marine,
+                  industrial, and decorative applications with dependable quality,
+                  long-lasting protection, and consistent finish.</p>
+               <div class="space24"></div>
+               <ul class="social_link">
+                  <li>
+                     <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                  </li>
+                  <li>
+                     <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                  </li>
+                  <li>
+                     <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                  </li>
+               </ul>
+            </div>
          </div>
          <div class="col-xl col-lg-4 col-md-6">
-           <div class="footer-widget-area">
-             <h3>Quick Links</h3>
-             <div class="space28"></div>
-             <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about-us.php">About Us</a></li>
-                <li><a href="products.php">Products</a></li>
-                <li><a href="blog.php">Blog</a></li>
-                <li><a href="contact.php">Contact Us</a></li>
-                <li><a href="request-quote.php">Request A Quote</a></li>
-           </ul>
-           </div>
+            <div class="footer-widget-area">
+               <h3>Quick Links</h3>
+               <div class="space28"></div>
+               <ul>
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="about-us.php">About Us</a></li>
+                  <li><a href="products.php">Products</a></li>
+                  <li><a href="blog.php">Blog</a></li>
+                  <li><a href="contact.php">Contact Us</a></li>
+                  <li><a href="request-quote.php">Request A Quote</a></li>
+               </ul>
+            </div>
          </div>
          <div class="col-xl col-lg-4 col-md-6">
-           <div class="footer-widget-hour">
-              <h4>Business Hours</h4>
-              <div class="space28"></div>
-              <ul>
+            <div class="footer-widget-hour">
+               <h4>Business Hours</h4>
+               <div class="space28"></div>
+               <ul>
                   <li><span><a class="f-date" href="javascript:void(0)">Mon–Fri:</a></span> <span><a href="javascript:void(0)">8:00 AM - 6:00 PM</a></span></li>
                   <li><span><a class="f-date" href="javascript:void(0)">Sat:</a></span> <span><a href="javascript:void(0)">9:00 AM - 2:00 PM</a></span></li>
                   <li><span><a class="f-date" href="javascript:void(0)">Sun:</a></span> <span><a href="javascript:void(0)">Closed</a></span></li>
-              </ul>
-           </div>
+               </ul>
+            </div>
          </div>
          <div class="col-xl col-lg-4 col-md-6">
             <div class="footer-area-gallery">
                <h3>Gallery </h3>
                <div class="space26"></div>
                <div class="row gx-2">
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-4">
-                    <div class="footer-img">
-                       <img src="assets/img/footer/footer1-img-(1).png" alt="">
-                    </div>
-                 </div>
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-4">
-                  <div class="footer-img">
-                       <img src="assets/img/footer/footer1-img-(2).png" alt="">
-                    </div>
-                 </div>
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-4">
-                  <div class="footer-img">
-                       <img src="assets/img/footer/footer1-img-(3).png" alt="">
-                    </div>
-                 </div>
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-4">
-                  <div class="footer-img">
-                       <img src="assets/img/footer/footer1-img-(4).png" alt="">
-                    </div>
-                 </div>
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-4">
-                  <div class="footer-img">
-                       <img src="assets/img/footer/footer1-img-(5).png" alt="">
-                    </div>
-                 </div>
-                 <div class="col-xl-4 col-lg-4 col-md-4 col-4">
-                  <div class="footer-img">
-                       <img src="assets/img/footer/footer1-img-(6).png" alt="">
-                    </div>
-                 </div>
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-4">
+                     <div class="footer-img">
+                        <img src="assets/img/footer/footer1-img-(1).png" alt="">
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-4">
+                     <div class="footer-img">
+                        <img src="assets/img/footer/footer1-img-(2).png" alt="">
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-4">
+                     <div class="footer-img">
+                        <img src="assets/img/footer/footer1-img-(3).png" alt="">
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-4">
+                     <div class="footer-img">
+                        <img src="assets/img/footer/footer1-img-(4).png" alt="">
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-4">
+                     <div class="footer-img">
+                        <img src="assets/img/footer/footer1-img-(5).png" alt="">
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-4 col-md-4 col-4">
+                     <div class="footer-img">
+                        <img src="assets/img/footer/footer1-img-(6).png" alt="">
+                     </div>
+                  </div>
                </div>
             </div>
          </div>
-        <div class="row footer1_bottom_fixer">
-           <div class="col-xl-4 col-lg-4 col-md-6">
-            <div class="footer-subscribe">
-               <h3>Subscribe Our Newsletter</h3>
-               <div class="space24"></div>
-               <form action="#">
-                    <input type="email" placeholder="Email Address">
-                    <button class="sub_arrow" type="submit"><i class="fa-solid fa-arrow-right"></i></button>
-                </form>
+         <div class="row footer1_bottom_fixer">
+            <div class="col-xl-4 col-lg-4 col-md-6">
+               <div class="footer-subscribe">
+                  <h3>Subscribe Our Newsletter</h3>
+                  <div class="space24"></div>
+                  <form action="#">
+                     <input type="email" placeholder="Email Address">
+                     <button class="sub_arrow" type="submit"><i class="fa-solid fa-arrow-right"></i></button>
+                  </form>
+               </div>
             </div>
-          </div>
-          <div class="col-xl-8 col-lg-8">
-              <div class="footer-mobile-area">
+            <div class="col-xl-8 col-lg-8">
+               <div class="footer-mobile-area">
                   <div class="footer-mobile-area-info">
-                      <h4>Email:</h4>
-                      <div class="space10"></div>
-                      <a href="#">info@oceanic.com</a>
+                     <h4>Email:</h4>
+                     <div class="space10"></div>
+                     <a href="#">info@oceanic.com</a>
                   </div>
-                   <div class="footer-mobile-area-info">
-                      <h4>Location:</h4>
-                      <div class="space10"></div>
-                      <a href="#">Ghanshyam enclve 501 Kandivali West</a>
+                  <div class="footer-mobile-area-info">
+                     <h4>Location:</h4>
+                     <div class="space10"></div>
+                     <a href="#">Ghanshyam enclve 501 Kandivali West</a>
                   </div>
-                   <div class="footer-mobile-area-info">
-                      <h4>Phone:</h4>
-                      <div class="space10"></div>
-                      <a href="#">+91 79001 04615</a>
+                  <div class="footer-mobile-area-info">
+                     <h4>Phone:</h4>
+                     <div class="space10"></div>
+                     <a href="#">+91 79001 04615</a>
                   </div>
-              </div>
-          </div>
-        </div>
-        <div class="space32"></div>
-            <div class="footer1-copyright-area">
-               <a href="#">© Copyright 2025 Oceanic . All rights reserved</a>
+               </div>
             </div>
+         </div>
+         <div class="space32"></div>
+         <div class="footer1-copyright-area">
+            <a href="#">© Copyright 2026 Oceanic . All rights reserved</a>
+         </div>
 
       </div>
-    </div>
+   </div>
 </div>
 <!--===== FOOTER AREA ENDS =======-->
 
 
-
-
 <!--===== SEARCH POP UP AREA =======-->
- <div class="header-search-form-wrapper">
-  <div class="tx-search-close tx-close"><i class="fa-solid fa-xmark"></i></div>
-  <div class="header-search-container">
+<div class="header-search-form-wrapper">
+   <div class="tx-search-close tx-close"><i class="fa-solid fa-xmark"></i></div>
+   <div class="header-search-container">
       <form role="search" class="search-form">
-      <input type="search"  class="search-field" placeholder="Search …" value="" name="s">
-      <button type="submit" class="search-submit"><img src="assets/img/icons/search1.php" alt=""></button>
+         <input type="search" class="search-field" placeholder="Search …" value="" name="s">
+         <button type="submit" class="search-submit"><img src="assets/img/icons/search1.php" alt=""></button>
       </form>
-  </div>
- </div>
+   </div>
+</div>
 <div class="body-overlay"></div>
 <!--===== SEARCH POP UP AREA =======-->
 
@@ -169,5 +166,4 @@
 
 </body>
 
-</html> 
-
+</html>
